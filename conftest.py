46ab1e17f229ab3f7381748b97c386663ea3edd1
mosaan <mosaan@users.noreply.github.com>
2025-05-07 23:50:13 +0900

    fix: Add return type annotation for anyio_backend fixture

--- conftest.py
+++ conftest.py
@@ -4,5 +4,5 @@ import pytest
 # 非同期テストを行う際に(asynioを使わず)trioのみを使用するための設定
 # この指定がないと、pytestがasyncioとtrio両方(厳密にはすべての対応したもの)を使用しようとする
 @pytest.fixture
-def anyio_backend():
+def anyio_backend() -> str:
     return "trio"
